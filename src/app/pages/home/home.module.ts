13e602d47f3ab3916408d22466391e7b6891543d
Alejandro Cueto Jimenez <alejandro.cueto.jimenez@dekra.com>
2023-04-25 17:56:06 +0200

    home page creation

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+import { HomePage } from './home.page';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { createTranslateLoader } from 'src/app/core/utils/translate';
+import { HttpClient } from '@angular/common/http';
+import { CoreModule } from 'src/app/core/core.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CoreModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
