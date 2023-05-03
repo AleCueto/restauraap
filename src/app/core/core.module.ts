09f638447edcb26a377c3d246519b929945efb4e
AleCueto <alecuetocontacto@gmail.com>
2023-05-03 19:44:41 +0200

    🧽CLEAN: remove components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,20 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TileComponent } from './components/tile/tile.component';
-import { FarmComponent } from './components/farm/farm.component';
-import { TileDetailComponent } from './components/tile-detail/tile-detail.component';
-import { StoreComponent } from './components/store/store.component';
-import { FarmeableSelectableComponent } from './components/farmeable-selectable/farmeable-selectable.component';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { createTranslateLoader } from './utils/translate';
-import { FarmListComponent } from './components/farm-list/farm-list.component';
 import { AboutComponent } from './components/about/about.component';
-import { FarmDetailedComponent } from './components/farm-detailed/farm-detailed.component';
-import { FarmeableCreatorComponent } from './components/farmeable-creator/farmeable-creator.component';
-import { FarmeableEditableComponent } from './components/farmeable-editable/farmeable-editable.component';
-import { FarmeableDetailedComponent } from './components/farmeable-detailed/farmeable-detailed.component';
 import { DishesComponent } from './components/dishes/dishes.component';
 import { DishesDetailedComponent } from './components/dishes-detailed/dishes-detailed.component';
 import { DishListItemComponent } from './components/dish-list-item/dish-list-item.component';
@@ -32,7 +22,7 @@ import { WaiterSelectableComponent } from './components/waiter-selectable/waiter
 
 
 @NgModule({
-  declarations: [TileComponent, FarmComponent, TileDetailComponent, StoreComponent, FarmeableSelectableComponent, FarmListComponent, AboutComponent, FarmDetailedComponent, FarmeableCreatorComponent, FarmeableEditableComponent, FarmeableDetailedComponent, DishesComponent, DishesDetailedComponent, DishListItemComponent, LoginComponent, RegisterComponent, MainComponent, TableComponent, TablesDetailedComponent, TableListItemComponent, WaitersComponent, WaiterListItemComponent, WaitersDetailedComponent, WaiterSelectableComponent],
+  declarations: [DishesComponent, DishesDetailedComponent, DishListItemComponent, LoginComponent, RegisterComponent, MainComponent, TableComponent, TablesDetailedComponent, TableListItemComponent, WaitersComponent, WaiterListItemComponent, WaitersDetailedComponent, WaiterSelectableComponent, AboutComponent],
   imports: [
     CommonModule,
     IonicModule,
@@ -54,19 +44,9 @@ import { WaiterSelectableComponent } from './components/waiter-selectable/waiter
     IonicModule,
     FormsModule,
     ReactiveFormsModule,
-    TileComponent,
-    FarmComponent,
-    TileDetailComponent,
-    StoreComponent,
-    FarmeableSelectableComponent,
     TranslateModule,
     HttpClientModule,
-    FarmListComponent,
     AboutComponent,
-    FarmDetailedComponent,
-    FarmeableCreatorComponent,
-    FarmeableEditableComponent,
-    FarmeableDetailedComponent,
     DishesComponent,
     DishesDetailedComponent,
     DishListItemComponent,
