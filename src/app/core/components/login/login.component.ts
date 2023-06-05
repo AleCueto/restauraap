b699342cc00aada4a4fe12a805ebbc2026a4fe8f
AleCueto <alecuetocontacto@gmail.com>
2023-06-05 20:06:43 +0200

    ⚙UPDATED: Login failed messages

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { error } from 'console';
 import { Router } from '@angular/router';
@@ -10,45 +10,64 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  formLogin:FormGroup
+  formLogin: FormGroup;
+  loginError: string = '';
 
   constructor(
     private userService: UserService,
-    private router: Router,
+    private router: Router
   ) {
-
     this.formLogin = new FormGroup({
-      email: new FormControl(),
-      password: new FormControl()
-    })
+      email: new FormControl('', [Validators.required, Validators.pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
-    console.log(this.formLogin.value)
-    this.userService.login(this.formLogin.value)
-    .then(response => {
-      console.log(response);
-      this.router.navigate(['/folder/Home']);
-    })
-    .catch(error => console.log(error));
+    console.log(this.formLogin.value);
+    if (this.formLogin.invalid) {
+      if (this.formLogin.controls['email'].errors!['pattern']) {
+        this.loginError = 'Por favor, ingresa un formato de correo electrónico válido.';
+      } else {
+        this.loginError = 'Por favor, ingresa el correo electrónico y la contraseña.';
+      }
+      return;
+    }
+
+    this.userService
+      .login(this.formLogin.value)
+      .then((response) => {
+        console.log(response);
+        this.router.navigate(['/folder/Home']);
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.status === 401) {
+          this.loginError = 'Error de autenticación. Verifica tus credenciales.';
+        } else if (error.status === 404) {
+          this.loginError = 'Usuario no encontrado.';
+        } else {
+          this.loginError = 'Error al iniciar sesión. Por favor, inténtalo de nuevo más tarde.';
+        }
+      });
   }
 
-  onClick(){
-    this.userService.loginWithGoogle()
-    .then(response =>{
-      console.log(response);
-      this.router.navigate(['/folder/Home']);
-    })
-    .catch(error => console.log(error))
+  onClick() {
+    this.userService
+      .loginWithGoogle()
+      .then((response) => {
+        console.log(response);
+        this.router.navigate(['/folder/Home']);
+      })
+      .catch((error) => {
+        console.log(error);
+        this.loginError = 'Error al iniciar sesión con Google. Por favor, inténtalo de nuevo más tarde.';
+      });
   }
 
-  goRegister(){
+  goRegister() {
     this.router.navigate(['/register']);
   }
-
-
 }
