c18c16f469f0072391043fcd21d2202bf28bfa22
AleCueto <alecuetocontacto@gmail.com>
2023-06-08 13:02:04 +0200

    🆙UPDATED: python

--- src/app/core/python/command.py
+++ src/app/core/python/command.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import csv
 import json
 from collections import OrderedDict
 
@@ -13,37 +12,40 @@ ruta_datos_csv = os.path.join(ruta_datos, 'datos.csv')
 with open(ruta_datos_json) as file:
     data = json.load(file, object_pairs_hook=OrderedDict)
 
-# Convertir las comandas en la estructura deseada
-datos = []
+# Crear listas para almacenar los datos de las comandas y los platos
+comandas_data = []
+platos_data = []
+
+# Iterar sobre las comandas y extraer los datos
 for comanda in data:
-    datos.append(["title", comanda.get("title", "")])
-    datos.append([])
-    datos[-1].extend(["Field", "Value"])
+    comanda_data = {}
+    platos = comanda.get("dishesList", [])
     for key, value in comanda.items():
         if key != "dishesList":
-            datos.append([key, str(value)])
+            comanda_data[key] = str(value)
+    
+    # Agregar los datos de la comanda a la lista de comandas
+    comandas_data.append(comanda_data)
 
-    # Agregar la lista de platos al final de la comanda
-    if "dishesList" in comanda:
-        datos.append([])
-        datos[-1].extend(["Index", "Name", "IdDish", "IdRestaurant", "Price", "Count"])
-        dish_count = {}
-        for dish in comanda["dishesList"]:
-            dish_id = dish.get("id", "")
-            dish_count[dish_id] = dish_count.get(dish_id, 0) + 1
-            if dish_count[dish_id] == 1:
-                row = [str(dish_count[dish_id])] + [str(dish.get(key, "")) for key in ["name", "id", "idRestaurant", "price"]] + [str(dish_count[dish_id])]
-                datos.append(row)
-            else:
-                for i in range(len(datos) - 1, 0, -1):
-                    if datos[i][2] == dish_id:
-                        datos[i][-1] = str(dish_count[dish_id])
-                        break
-        datos.append([])
+    # Agregar los datos de los platos a la lista de platos
+    for index, plato in enumerate(platos, start=1):
+        plato_data = {
+            "Comanda": comanda_data["title"],
+            "Index": index,
+            "Name": plato.get("name", ""),
+            "IdDish": plato.get("id", ""),
+            "IdRestaurant": plato.get("idRestaurant", ""),
+            "Price": plato.get("price", ""),
+            "Count": plato.get("count", "")
+        }
+        platos_data.append(plato_data)
 
-# Escribir los datos en el archivo CSV
-with open(ruta_datos_csv, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(datos)
+# Crear DataFrames para las comandas y los platos
+comandas_df = pd.DataFrame(comandas_data)
+platos_df = pd.DataFrame(platos_data)
+
+# Guardar los DataFrames en el archivo CSV
+comandas_df.to_csv(ruta_datos_csv, index=False)
+platos_df.to_csv(ruta_datos_csv, mode='a', index=False, header=False)
 
 print("Archivo CSV creado con éxito.")
